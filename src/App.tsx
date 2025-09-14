513dbc1a9970c81b1aa3640c76824b343400020d
Gabriel Andrade <gabriel.santana@gmail.com>
2025-09-14 12:20:53 -0300

    Código finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import Banner from "./components/Banner";
 import Header from "./components/Header";
+import MovieSection from "./components/MovieSection";
+import Newsletter from "./components/Newsletter";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-  <>
-    <Header/>
-    <Banner src={"./banner.png"} alt={"Banner"} />
-  </>
-  )
+    <>
+      <Header />
+      <Banner src="./banner.png" alt="Banner" />
+      <MovieSection />
+      <Banner src="./combo.png" alt="Combo" />
+      <Newsletter />
+      <Footer />
+    </>
+  );
 }
-
 export default App
